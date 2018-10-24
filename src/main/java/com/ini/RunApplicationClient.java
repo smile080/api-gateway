9f0c2d217f3070ea970d23945afda72467012c2d
chen_jian <chenjian190000774@126.com>
2018-10-25 06:31:24 +0800

    20181025  6:31

--- src/main/java/com/ini/RunApplicationClient.java
+++ src/main/java/com/ini/RunApplicationClient.java
@@ -3,6 +3,9 @@ package com.ini;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.ini.filter.AccessFilter;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -11,4 +14,10 @@ public class RunApplicationClient {
 	public static void main(String[] args) {
 		SpringApplication.run(RunApplicationClient.class, args);
 	}
+	
+	@Bean
+	public AccessFilter accessFiter(){
+		return new AccessFilter();
+	}
+	
 }
---
+++ src/main/java/com/ini/filter/AccessFilter.java
@@ -0,0 +1,48 @@
+package com.ini.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+public class AccessFilter extends ZuulFilter{
+
+	private static final Logger log = LoggerFactory.getLogger(AccessFilter.class);
+	
+	@Override
+	public Object run() {
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
+		
+		log.info("send {}  request to {}", request.getMethod(), request.getRequestURL().toString());
+		
+		Object accessToken = request.getParameter("accessToken");
+		if(accessToken == null){
+			log.warn("access token is empty");
+			ctx.setSendZuulResponse(false);
+			ctx.setResponseStatusCode(401);
+			return null;
+		}
+		
+		return null;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public int filterOrder() {
+		return 0;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+}
