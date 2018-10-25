4c4a390d38e21c14aaf7d335592b7d4585e1f4cc
chen_jian <chenjian190000774@126.com>
2018-10-25 17:31:13 +0800

    20181025 17:31

--- src/main/java/com/ini/RunApplicationGateWayClient.java
+++ src/main/java/com/ini/RunApplicationGateWayClient.java
@@ -3,7 +3,6 @@ package com.ini;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
 
 import com.ini.filter.AccessFilter;
 
@@ -15,7 +14,7 @@ public class RunApplicationGateWayClient {
 		SpringApplication.run(RunApplicationGateWayClient.class, args);
 	}
 	
-	@Bean
+
 	public AccessFilter accessFiter(){
 		return new AccessFilter();
 	}
